13b6cca3dc29c2c551ea4c05dd9c839c2e42eab5
justgaret23 <sheehan.garrett23@gmail.com>
2021-04-15 11:13:44 -0500

    db

--- SnakeCase2Playtest/game.js
+++ SnakeCase2Playtest/game.js
@@ -666,7 +666,6 @@ let G = (function (){
 			// DO NOT MODIFY THIS FUNCTION CALL
 			// except as instructed
 
-			/*
 			const TEAM = "pix";
 
 			// This code should be the last thing
@@ -680,9 +679,7 @@ let G = (function (){
 				}
 				PS.dbEvent( TEAM, "startup", user );
 				PS.dbSend( TEAM, PS.CURRENT, { discard : true } );
-			}, { active : false } );
-
-			 */
+			}, { active : true } );
 
 			// Change the false in the final line above to true
 			// before deploying the code to your Web site.
